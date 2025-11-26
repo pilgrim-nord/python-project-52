5e3b85c03cc2febd8eb4431bce981bd5b8fcd1c4
pilgrim-nord <dmelekhov@gmail.com>
2025-11-26 16:43:57 +0300

    update wsgi

--- task_manager/wsgi.py
+++ task_manager/wsgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 sys.path.insert(0, '/project/.venv/lib/python3.13/site-packages')
