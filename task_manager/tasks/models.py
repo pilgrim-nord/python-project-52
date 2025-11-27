a331c87645ec6c1fca67c86180e7f7f642f14434
pilgrim-nord <dmelekhov@gmail.com>
2025-11-27 18:21:31 +0300

    update forms.py

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Task(models.Model):
-    name = models.CharField(max_length=200, verbose_name="Название")
+    name = models.CharField(max_length=200, verbose_name="Имя")
     description = models.TextField(verbose_name="Описание")
     author = models.ForeignKey(
         User,
