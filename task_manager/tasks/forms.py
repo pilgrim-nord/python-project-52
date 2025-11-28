d3c834e0f2f6009fe9f966be3f4f54d2796958f1
pilgrim-nord <dmelekhov@gmail.com>
2025-11-28 17:49:23 +0300

    update tasks form

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -1,16 +1,39 @@
+# from django import forms
+# from django.contrib.auth.models import User
+#
+# from .models import Task
+#
+# class TaskForm(forms.ModelForm):
+#     class Meta:
+#         model = Task
+#         fields = ['name', 'description', 'status', 'executor', 'labels']
+#         labels = {
+#             'name': 'Имя',
+#             'description': 'Описание',
+#             'status': 'Статус',
+#             'executor': 'Исполнитель',
+#             'labels': 'Метки',
+#         }
 from django import forms
-from django.contrib.auth.models import User
 
 from .models import Task
+from django.contrib.auth.models import User
+from task_manager.statuses.models import Status
+from task_manager.labels.models import Label
+
 
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['name', 'description', 'status', 'executor', 'labels']
-        labels = {
-            'name': 'Имя',
-            'description': 'Описание',
-            'status': 'Статус',
-            'executor': 'Исполнитель',
-            'labels': 'Метки',
-        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Querysets
+        self.fields['executor'].queryset = User.objects.all()
+        self.fields['status'].queryset = Status.objects.all()
+        self.fields['labels'].queryset = Label.objects.all()
+        # label для select по исполнителю
+        self.fields['executor'].label_from_instance = (
+            lambda obj: obj.get_full_name()
+        )
