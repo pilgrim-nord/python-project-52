bc29ae78cd9320aaf3be9715986b5cdf6ef2a862
pilgrim-nord <dmelekhov@gmail.com>
2025-11-28 18:21:53 +0300

    update tasks template

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -1,19 +1,4 @@
-# from django import forms
-# from django.contrib.auth.models import User
-#
-# from .models import Task
-#
-# class TaskForm(forms.ModelForm):
-#     class Meta:
-#         model = Task
-#         fields = ['name', 'description', 'status', 'executor', 'labels']
-#         labels = {
-#             'name': 'Имя',
-#             'description': 'Описание',
-#             'status': 'Статус',
-#             'executor': 'Исполнитель',
-#             'labels': 'Метки',
-#         }
+
 from django import forms
 
 from .models import Task
